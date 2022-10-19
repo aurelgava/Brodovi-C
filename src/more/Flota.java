f6111534f9f8b4be0b5bfcd8a7eac355dcc897bd
stancevski42 <10219lukastancevski@etsntesla.edu.rs>
2022-10-19 09:56:07 +0200

    provera

---
+++ src/more/Flota.java
@@ -0,0 +1,27 @@
+package more;
+
+public class Flota {
+
+    public static class Element{
+        Brod b;
+        Element next;
+
+        public Element(Brod b){
+            this.b = b;
+        }
+
+    }
+
+    private String naziv;
+    private char vrstaFlote; //K - kraljevska,   G - gusari
+
+//aa
+    Flota(String x){
+        this.naziv = x;
+    }
+
+    public void dodaj(Brod a){
+
+    }
+
+}
